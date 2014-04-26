cddf3a718368fe476a458dcf581b40f4982abe9f
Thomas Brus <thomas.brus@me.com>
2014-04-26 16:14:40 +0200

    Added yard doc for GADDAG::Arc

--- lib/gaddag/arc.rb
+++ lib/gaddag/arc.rb
@@ -3,20 +3,29 @@
 require 'set'
 
 class GADDAG
+
+  # Represents an arc pointing to a destination node with optional final letters
   class Arc
     # The destination node
     attr_reader :destination
 
-    # The letters which complete a word after following this arc.
+    # A set of letters which form a word after being appended to the letter path
     attr_reader :final_letters
 
+    # Initializes a GADDAG arc
+    # @param destination [Node] the destination node
+    # @return [Arc]
     def initialize(destination)
       @destination = destination
       @final_letters = Set.new
     end
 
+    # Adds a final letter to the arc. A final letter is a letter that,
+    # when appended to the letter path, forms a valid word.
+    # @param letter [String] the letter that is to be marked as final
     def add_final_letter(letter)
       @final_letters.add(letter)
     end
   end
+
 end
