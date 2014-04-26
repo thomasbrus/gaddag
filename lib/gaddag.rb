e7425765bdceb49bd47315455bcc8fc4af1cbc62
Thomas Brus <thomas.brus@me.com>
2014-04-26 16:17:03 +0200

    Added GADDAG#find method that finds words by substring

--- lib/gaddag.rb
+++ lib/gaddag.rb
@@ -2,39 +2,43 @@
 
 require_relative 'gaddag/arc'
 require_relative 'gaddag/node'
+require_relative 'gaddag/path'
+require_relative 'gaddag/word'
 
+# Implementation of the GADDAG data structure
 class GADDAG
-  DELIMITER = '♢'.freeze
+  # The root node
+  attr_reader :root
 
+  # Initializes a GADDAG
+  # @return [GADDAG]
   def initialize
     @root = Node.new
   end
 
+  # Adds a word to the GADDAG
+  # @param word [String] the word to be added
+  # @return [GADDAG] the GADDAG instance itself
   def add(word)
     @root.create_final_path(word.reverse.chars)
 
-    generate_delimited_words(word).each do |word|
-      reversed_prefix, suffix = word.split(DELIMITER)
-      last_prefix_node = @root.create_path(reversed_prefix.chars)
-      last_prefix_node.create_final_path([DELIMITER] + suffix.chars)
+    Word.new(word.chars).to_paths.each do |path|
+      node = @root.create_path(path.reversed_prefix_letters)
+      node.create_final_path([Path::DELIMITER] + path.suffix_letters)
     end
+
+    self
   end
 
-  # def remove(word)
-  #   ...
-  # end
-
-  # def find(substring)
-  #   ...
-  # end
-
-  private
-
-  def generate_delimited_words(word)
-    1.upto(word.length - 1).map do |index|
-      reversed_prefix = word.slice(0, index).reverse
-      suffix = word.slice(index, word.length - index)
-      reversed_prefix << DELIMITER << suffix
+  # Finds all words that contain the given substring
+  # @param substring [String] the substring to search for
+  # @return [Array<Word>] all matching words
+  def find(substring)
+    reversed_prefix_letters = substring.reverse.chars
+    @root.follow_arcs(reversed_prefix_letters).find_paths.map do |path|
+      Path.new(reversed_prefix_letters + path).to_word
     end
+  rescue KeyError
+    []
   end
 end
