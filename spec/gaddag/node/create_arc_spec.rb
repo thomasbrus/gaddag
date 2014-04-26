96eaa7441af12b63265651f056003ebb9a7e76c7
Thomas Brus <thomas.brus@me.com>
2014-04-26 23:49:54 +0200

    Minor change, bit more intension revealing test

--- spec/gaddag/node/create_arc_spec.rb
+++ spec/gaddag/node/create_arc_spec.rb
@@ -2,11 +2,12 @@ require 'gaddag'
 
 describe GADDAG::Node, '#create_arc' do
   subject { GADDAG::Node.new }
+  let(:letter) { 'L' }
   let(:destination) { GADDAG::Node.new }
   let(:another_destination) { GADDAG::Node.new }
 
   context 'when created for the first time' do
-    let(:arc) { subject.create_arc('L', destination) }
+    let(:arc) { subject.create_arc(letter, destination) }
 
     it 'returns the newly created arc' do
       expect(arc).to eq(GADDAG::Arc.new(destination))
@@ -14,8 +15,8 @@ describe GADDAG::Node, '#create_arc' do
   end
 
   context 'when created again for the same letter' do
-    let(:first_arc) { subject.create_arc('L') }
-    let(:second_arc) { subject.create_arc('L', destination) }
+    let(:first_arc) { subject.create_arc(letter) }
+    let(:second_arc) { subject.create_arc(letter, destination) }
 
     it 'returns the first arc each time' do
       expect(second_arc).to equal(first_arc)
@@ -27,19 +28,19 @@ describe GADDAG::Node, '#create_arc' do
   end
 
   context 'when not provided with a specific destination' do
-    before { subject.create_arc('L') }
+    before { subject.create_arc(letter) }
 
     it 'creates an arc for the given letter pointing to a newly created destination' do
-      expect(subject.follow_arc('L')).to eq(GADDAG::Node.new)
-      expect(subject.follow_arc('L')).to_not equal(destination)
+      expect(subject.follow_arc(letter)).to eq(GADDAG::Node.new)
+      expect(subject.follow_arc(letter)).to_not equal(GADDAG::Node.new)
     end
   end
 
   context 'when provided with a specific destination' do
-    before { subject.create_arc('L', destination) }
+    before { subject.create_arc(letter, destination) }
 
     it 'creates an arc for the given letter pointing to that destination' do
-      expect(subject.follow_arc('L')).to equal(destination)
+      expect(subject.follow_arc(letter)).to equal(destination)
     end
   end
 end
