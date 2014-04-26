f0f95a76ca5cf5cf42a9093feb4c988b166c31cd
Thomas Brus <thomas.brus@me.com>
2014-04-26 16:12:56 +0200

    Added Word class that represents a GADDAG word

---
+++ lib/gaddag/word.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+
+class GADDAG
+
+  # Represents a word in the GADDAG data structure
+  class Word
+    # The letters that make up this word
+    attr_reader :letters
+
+    # Initializes a GADDAG word
+    # @param letters [Array<String>] an ordered list of letters of which
+    # the word consists
+    # @return [Word]
+    def initialize(letters)
+      @letters = letters
+    end
+
+    # Returns the word as string
+    # @return a string representation of the word
+    def to_s
+      @letters.join
+    end
+
+    # Construct a list of GADDAG paths from this word
+    # @return [Array<Path>] a list of paths, each containing a reversed prefix,
+    # a delimiter, and an optional suffix: REV(PREFIX) ♢ SUFFIX
+    def to_paths
+      1.upto(letters.length - 1).map do |index|
+        reversed_prefix = @letters.slice(0, index).reverse
+        suffix = @letters.slice(index, @letters.count - index)
+        Path.new(reversed_prefix + [Path::DELIMITER] + suffix)
+      end
+    end
+  end
+
+end
