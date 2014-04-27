dce8b485da1fac6521208449350233828c7fdef0
Thomas Brus <thomas.brus@me.com>
2014-04-27 15:34:58 +0200

    Moved let/subject inline

--- spec/unit/gaddag/node/create_final_arc_spec.rb
+++ spec/unit/gaddag/node/create_final_arc_spec.rb
@@ -3,11 +3,11 @@
 require 'gaddag'
 
 describe GADDAG::Node, '#create_final_arc' do
-  subject { GADDAG::Node.new }
-
-  let(:final_letter) { 'Z' }
-
   it_behaves_like 'GADDAG::Node#create_arc' do
+    subject { GADDAG::Node.new }
+
+    let(:final_letter) { 'Z' }
+
     let(:create_arc) do
       ->(*args) { subject.create_final_arc(*args.insert(1, final_letter)) }
     end
