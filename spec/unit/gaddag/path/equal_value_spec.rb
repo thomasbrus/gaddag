d07aa3f57250ee5e0a7a89d70bc240429770cc84
Thomas Brus <thomas.brus@me.com>
2014-04-27 15:34:45 +0200

    Simplified #== specs

--- spec/unit/gaddag/path/equal_value_spec.rb
+++ spec/unit/gaddag/path/equal_value_spec.rb
@@ -3,23 +3,15 @@
 require 'gaddag'
 
 describe GADDAG::Path, '#==' do
-  context 'when two paths contain the same letters' do
-    context 'when the letters are in the same order' do
-      let(:path) { GADDAG::Path.new(%w(B ♢ R E A K)) }
-      let(:other_path) { GADDAG::Path.new(%w(B ♢ R E A K)) }
-      specify { expect(path).to eq(other_path) }
-    end
-
-    context 'when the letters are in a different order' do
-      let(:path) { GADDAG::Path.new(%w(B ♢ R E A K)) }
-      let(:other_path) { GADDAG::Path.new(%w(R B ♢ E A K)) }
-      specify { expect(path).to_not eq(other_path) }
-    end
+  context 'when two paths have the same letters' do
+    let(:path) { GADDAG::Path.new(%w[B ♢ R E A K]) }
+    let(:other_path) { GADDAG::Path.new(%w[B ♢ R E A K]) }
+    specify { expect(path).to eq(other_path) }
   end
 
-  context 'when two paths contain different letters' do
-    let(:path) { GADDAG::Path.new(%w(B ♢ R E A K)) }
-    let(:other_path) { GADDAG::Path.new(%w(C ♢ A R E)) }
+  context 'when two paths have different letters' do
+    let(:path) { GADDAG::Path.new(%w[B ♢ R E A K]) }
+    let(:other_path) { GADDAG::Path.new(%w[C ♢ A R E]) }
     specify { expect(path).to_not eq(other_path) }
   end
 end
--- spec/unit/gaddag/word/equal_value_spec.rb
+++ spec/unit/gaddag/word/equal_value_spec.rb
@@ -3,23 +3,15 @@
 require 'gaddag'
 
 describe GADDAG::Word, '#==' do
-  context 'when two words contain the same letters' do
-    context 'when the letters are in the same order' do
-      let(:word) { GADDAG::Word.new(%w(B R E A K)) }
-      let(:other_word) { GADDAG::Word.new(%w(B R E A K)) }
-      specify { expect(word).to eq(other_word) }
-    end
-
-    context 'when the letters are in a different order' do
-      let(:word) { GADDAG::Word.new(%w(B R E A K)) }
-      let(:other_word) { GADDAG::Word.new(%w(R E B A K)) }
-      specify { expect(word).to_not eq(other_word) }
-    end
+  context 'when two words have the same letters' do
+    let(:word) { GADDAG::Word.new(%w[B R E A K]) }
+    let(:other_word) { GADDAG::Word.new(%w[B R E A K]) }
+    specify { expect(word).to eq(other_word) }
   end
 
-  context 'when two words contain different letters' do
-    let(:word) { GADDAG::Word.new(%w(B R E A K)) }
-    let(:other_word) { GADDAG::Word.new(%w(C A R E)) }
+  context 'when two words have different letters' do
+    let(:word) { GADDAG::Word.new(%w[B R E A K]) }
+    let(:other_word) { GADDAG::Word.new(%w[C A R E]) }
     specify { expect(word).to_not eq(other_word) }
   end
 end
