b99af5519583163695b4d4c1572225867d17dbc6
Thomas Brus <thomas.brus@me.com>
2014-04-28 02:36:21 +0200

    Renamed shared examples to shared behaviour

--- spec/shared/unit/gaddag/node/create_arc_behaviour.rb
+++ spec/shared/unit/gaddag/node/create_arc_behaviour.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-shared_examples 'GADDAG::Node#create_arc' do
+shared_examples 'GADDAG::Node#create_arc/behaviour' do
   let(:letter) { 'L' }
   let(:destination) { GADDAG::Node.new }
 
