c4d20143965338be2ca7d54ae6dbd646e6ebbc88
Thomas Brus <thomas.brus@me.com>
2014-04-28 02:39:48 +0200

    Added #create_final_path spec + behavior

---
+++ spec/shared/unit/gaddag/node/create_final_path_behaviour.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+shared_examples 'GADDAG::Node#create_final_path/behaviour' do
+  include_context 'GADDAG::Node#create_path/context'
+
+  let!(:final_node) { subject.create_final_path(letters) }
+
+  let(:letters_without_last_one) { letters[0..-2] }
+  let(:letters_without_last_two) { letters[0..-3]}
+
+  let(:last_letter) { letters[-1] }
+  let(:second_last_letter) { letters[-2] }
+
+  it 'creates the path for the given letters up till the second last letter' do
+    expect { follow_arcs(subject, letters_without_last_one) }.to_not raise_error
+  end
+
+  it 'does not create an arc for the last letter' do
+    expect { follow_arcs(subject, letters) }.to raise_error(KeyError)
+  end
+
+  it 'adds the last letter to the last arc as final letter' do
+    expect(follow_arcs(subject, letters_without_last_two).outgoing_arcs).to eq({
+      second_last_letter => GADDAG::Arc.new(GADDAG::Node.new).tap do |arc|
+        arc.add_final_letter(last_letter)
+      end
+    })
+  end
+
+  it 'returns the last created node' do
+    expect(final_node).to equal(follow_arcs(subject, letters_without_last_one))
+  end
+end
---
+++ spec/unit/gaddag/node/create_final_path_spec.rb
@@ -0,0 +1,44 @@
+# encoding: utf-8
+
+require 'gaddag'
+require 'shared/unit/gaddag/node/create_path_context'
+require 'shared/unit/gaddag/node/create_final_path_behaviour'
+
+describe GADDAG::Node, '#create_final_path' do
+  context 'when not given a list of destination nodes' do
+    it_behaves_like 'GADDAG::Node#create_final_path/behaviour'
+  end
+
+  context 'when given a list of destination nodes' do
+    it_behaves_like 'GADDAG::Node#create_final_path/behaviour' do
+      let(:letters) { %w[B ♢ R E A K] }
+
+      let!(:destinations) { 6.times.map { GADDAG::Node.new } }
+      let!(:final_node) { subject.create_final_path(letters, destinations) }
+
+      it 'creates the path for the given letters through the given destination nodes
+          while ommitting the last letter' do
+        expect(follow_arcs(subject, %w[B])).to equal(destinations[0])
+        expect(follow_arcs(subject, %w[B ♢])).to equal(destinations[1])
+        expect(follow_arcs(subject, %w[B ♢ R])).to equal(destinations[2])
+        expect(follow_arcs(subject, %w[B ♢ R E])).to equal(destinations[3])
+        expect(follow_arcs(subject, %w[B ♢ R E A])).to equal(destinations[4])
+      end
+    end
+  end
+
+  context 'when given a shortened list of destination nodes' do
+    it_behaves_like 'GADDAG::Node#create_final_path/behaviour' do
+      let(:letters) { %w[B ♢ R E A K] }
+
+      let(:destinations) { 3.times.map { GADDAG::Node.new } }
+      let!(:final_node) { subject.create_final_path(letters, destinations) }
+
+      it 'creates a path through the shortened list of destination nodes' do
+        expect(follow_arcs(subject, %w[B])).to equal(destinations[0])
+        expect(follow_arcs(subject, %w[B ♢])).to equal(destinations[1])
+        expect(follow_arcs(subject, %w[B ♢ R])).to equal(destinations[2])
+      end
+    end
+  end
+end
