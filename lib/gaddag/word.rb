07f57bc055c4d8890ca75db0cde4c990d0058390
Thomas Brus <thomas.brus@me.com>
2014-05-13 01:24:52 +0200

    Fixed doc typo

--- lib/gaddag/word.rb
+++ lib/gaddag/word.rb
@@ -24,7 +24,7 @@ class GADDAG
       @letters.join
     end
 
-    # Construct a list of delimited GADDAG paths from this word
+    # Constructs a list of delimited GADDAG paths from this word
     # @return [Array<Path>] a list of paths, each containing a reversed prefix,
     # a delimiter, and a suffix: REV(PREFIX) ♢ SUFFIX
     def to_delimited_paths
