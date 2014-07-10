36a1ba6fe9d38240906fd51f24048f9fc54e6a8c
Thomas Brus <thomas.brus@me.com>
2014-07-11 00:38:39 +0200

    Updated specs to use rspec 3 syntax

--- spec/unit/gaddag/path/include_delimiter_spec.rb
+++ spec/unit/gaddag/path/include_delimiter_spec.rb
@@ -5,11 +5,11 @@ require 'gaddag'
 describe GADDAG::Path, '#include_delimiter?' do
   context 'when the path includes a delimiter' do
     subject { GADDAG::Path.new(%w[R B] + [GADDAG::Path::DELIMITER] + %w[E A K]) }
-    specify { expect(subject.include_delimiter?).to be_true }
+    specify { expect(subject.include_delimiter?).to be_truthy }
   end
 
   context 'when the path does not include a delimiter' do
     subject { GADDAG::Path.new(%w[K A E R B]) }
-    specify { expect(subject.include_delimiter?).to be_false }
+    specify { expect(subject.include_delimiter?).to be_falsey }
   end
 end
